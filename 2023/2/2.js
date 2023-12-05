b146cdc0e264382c8c4d56d47f81e5fe39f17769
Sam Dushay <sdushay@gmail.com>
2023-12-05 09:06:14 -0500

    day 2

---
+++ 2023/2/2.js
@@ -0,0 +1,66 @@
+const fsp = require('node:fs/promises');
+
+myFileReader();
+
+async function myFileReader() {
+    const file = await fsp.open('./input.txt');
+
+    const maxes = {
+        red: 12,
+        green: 13,
+        blue: 14
+    }
+    const gamesArr = [null];
+
+    for await (const line of file.readLines()) {
+        const arr = line.split(/[\:|\;]\s*/g)
+
+        // [
+        //     'Game 2',
+        //     '1 blue, 2 green',
+        //     '3 green, 4 blue, 1 red',
+        //     '1 green, 1 blue'
+        // ]
+        const gameObj = {}
+
+        for (let i = 1; i < arr.length; i++) {
+            const hand = arr[i];
+            const colors = hand.split(',');
+            colors.forEach((c) => {
+                const q = parseInt(c.match(/\d+/)[0])
+                const label = c.match(/[a-zA-Z]+/)[0]
+                if (gameObj.hasOwnProperty(label)) {
+                    if (gameObj[label] < q) {
+                        gameObj[label] = q
+                    }
+                } else {
+                    gameObj[label] = q
+                }
+            })
+        }
+        gamesArr.push(gameObj)
+    }
+    // console.log('====')
+    // console.log(sum)
+    let sum = 0;
+    gamesArr.forEach((game, i) => {
+        let exceeds = false;
+        if (game) {
+            console.log(game)
+            for (const color in game) {
+                const maxFound = game[color]
+                if (maxFound > maxes[color]) {
+                    exceeds = true
+                }
+            }
+            game['exceeds'] = exceeds;
+            if (!exceeds) {
+                sum += i;
+            }
+        }
+    })
+    console.log(gamesArr)
+    console.log(sum)
+
+
+}
